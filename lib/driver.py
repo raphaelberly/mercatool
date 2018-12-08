aa32692fd065a147be1eb13a0365919b0cd6869d
Raphaël Berly <raphaelberly@gmail.com>
2018-12-08 01:02:54 +0100

    Parametrized run_driver function

--- lib/driver.py
+++ lib/driver.py
@@ -8,22 +8,21 @@ from selenium.webdriver.chrome.options import Options
 
 
 @contextmanager
-def run_driver():
+def run_driver(docker_image, driver_lang='en', driver_size=None):
 
     print('Starting docker container...')
     client = docker.from_env()
-    client.images.pull('selenium/standalone-chrome:3.141.59-copernicium')
-    container = client.containers.run('selenium/standalone-chrome:3.141.59-copernicium',
-                                      detach=True, ports={'4444/tcp': 4444})
+    container = client.containers.run(docker_image, detach=True, ports={'4444/tcp': 4444})
 
     try:
         print('Starting driver...')
-        sleep(6)  # TRY AND WAIT HERE INSTEAD OF WAIT
+        sleep(12)  # TODO: REPLACE THIS WITH: WHILE... TRY...
         chrome_options = Options()
-        chrome_options.add_argument('--window-size=300,800')
-        chrome_options.add_argument('--lang=en')
-        chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en'})
-        yield webdriver.Remote('http://127.0.0.1:4444/wd/hub', DesiredCapabilities.CHROME,
+        chrome_options.add_argument(f'--lang={driver_lang}')
+        chrome_options.add_experimental_option('prefs', {'intl.accept_languages': driver_lang})
+        if driver_size:
+            chrome_options.add_argument('--window-size={0},{1}'.format(*driver_size))
+        yield webdriver.Remote('http://localhost:4444/wd/hub', DesiredCapabilities.CHROME,
                                options=chrome_options)
 
     finally:
