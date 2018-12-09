337423572a1117646be6e7c80fa4863174745057
Raphaël Berly <raphaelberly@gmail.com>
2018-12-09 23:50:40 +0100

    Add "ignored" context manager

--- lib/tools.py
+++ lib/tools.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 
 import numpy as np
 import pandas as pd
@@ -12,3 +13,11 @@ def fraction_to_float(string):
     assert len(aux) in [1, 2], 'The provided string ("{}") is not a number or a fraction'.format(string)
     # Output the computed ratio value as a string
     return aux[0] if len(aux) == 1 else str(float(aux[0])/float(aux[1]))
+
+
+@contextmanager
+def ignored(*exceptions):
+    try:
+        yield
+    except exceptions:
+        pass
