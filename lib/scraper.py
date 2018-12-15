aeac8de3b3de1105049bdd646dceb533e078df2b
Raphaël Berly <raphaelberly@gmail.com>
2018-12-15 18:10:42 +0100

    Various renamings, and some code hygiene stuff

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-import os
 from contextlib import contextmanager
 from time import sleep
 
 import psycopg2
 from selenium.common.exceptions import NoSuchElementException
 
-import pandas as pd
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.action_chains import ActionChains
 
@@ -33,15 +31,20 @@ class BasicScraper(object):
 
 class Scraper(BasicScraper):
 
-    def __init__(self, driver, credentials, urls, classes, maps=None, transform=None):
+    def __init__(self, day, season, driver, table_name, credentials, urls, classes,
+                 mappings=None, transformations=None):
 
         super().__init__(driver)
+
+        self.day = day
+        self.season = season
+        self.table_name = table_name
 
         self.credentials = credentials
         self.urls = urls
         self.classes = classes
-        self.maps = maps
-        self.transform = transform
+        self.mappings = mappings
+        self.transformations = transformations
 
     # GET PARAMS OF THE BEAUTIFUL SOUP FIND(ALL) FUNCTIONS
     def get_find_args(self, class_name):
@@ -64,18 +67,18 @@ class Scraper(BasicScraper):
         sleep(2)
 
     def _apply_maps(self, dict_item):
-        if not self.maps:
+        if not self.mappings:
             return dict_item
-        for key, map in self.maps.items():
+        for key, map in self.mappings.items():
             if key in dict_item:
                 old_value = dict_item[key]
                 dict_item[key] = map.get(old_value, old_value)
         return dict_item
 
     def _apply_transformations(self, dict_item):
-        if not self.transform:
+        if not self.transformations:
             return dict_item
-        for key, func in self.transform.items():
+        for key, func in self.transformations.items():
             if key in dict_item:
                 dict_item[key] = resolve(func)(dict_item[key])
         return dict_item
@@ -91,62 +94,55 @@ class Scraper(BasicScraper):
         else: conn.commit()
         finally: conn.close()
 
-    def erase_day_from_table(self, schema, table, day, season):
+    def erase_day_from_table(self):
         print('Deleting existing data for this day and season...')
         with self._connect_to_db(**self.credentials['db']) as conn:
             cur = conn.cursor()
-            cur.execute(f"DELETE FROM {schema}.{table} WHERE day = {day} AND season = '{season}'")
+            query = f"DELETE FROM {self.table_name} WHERE day={self.day} AND season='{self.season}'"
+            cur.execute(query)
             cur.close()
 
     @staticmethod
-    def _get_insert_query(schema_name, table_name, player_dict):
+    def _get_insert_query(table_name, player_dict):
         key_str = ','.join(player_dict.keys())
         val_str = "'{}'".format("','".join([str(val) for val in player_dict.values()]))
-        return f'INSERT INTO {schema_name}.{table_name} ({key_str}) VALUES ({val_str});'
+        return f'INSERT INTO {table_name} ({key_str}) VALUES ({val_str});'
 
     # EMPTY A DAY IF
-    def insert_values_into_table(self, values_generator, schema, table, batch_size=BATCH_SIZE):
-        print('Deleting existing data for this day and season...')
+    def insert_values_into_table(self, values_generator, batch_size=BATCH_SIZE):
+        print('Inserting new data for this day and season...')
         with self._connect_to_db(**self.credentials['db']) as conn:
             i, queries = 0, []
             for value in values_generator:
                 i += 1
-                queries.append(self._get_insert_query(schema, table, value))
+                queries.append(self._get_insert_query(self.table_name, value))
                 if i == batch_size:
                     cur = conn.cursor()
                     cur.execute('\n'.join(queries))
                     cur.close()
+                    print(len(queries))
                     i, queries = 0, []
 
 
-class PlayerScraper(Scraper):
-
-    def __init__(self, day, season, driver, credentials, urls, classes,
-                 maps=None, transform=None):
-
-        super().__init__(driver, credentials, urls, classes, maps, transform)
-
-        self.day = day
-        self.season = season
+class RatingScraper(Scraper):
 
     # PARSE GAME DETAILS
-    def get_player_df(self):
-        print('Getting players DataFrame...')
+    def get_rating_values(self):
+        print('Getting players ratings...')
         self.get_page(self.urls['mercato'])
         # Parse overall code
         soup = self.get_soup()
         # Get all lines (one per player)
-        lines = soup.findAll(**self.get_find_args('player_line'))
+        lines = soup.findAll(**self.get_find_args('line'))
         # Parse all lines
-        to_extract = ['player_name', 'player_position', 'player_team', 'player_rating']
+        to_extract = ['player', 'position', 'team', 'rating']
         for line in lines:
             # Remove player first name
-            _ = line.find(**self.get_find_args('player_first_name')).extract()
+            _ = line.find(**self.get_find_args('first_name')).extract()
             # Parse line
             player = {'day': self.day, 'season': self.season}
             for item in to_extract:
-                name = item[len('player_'):]
-                player[name] = line.find(**self.get_find_args(item)).text.replace('\xa0', '')
+                player[item] = line.find(**self.get_find_args(item)).text.replace('\xa0', '')
             player = self._apply_maps(player)
             player = self._apply_transformations(player)
             yield player
@@ -154,13 +150,10 @@ class PlayerScraper(Scraper):
 
 class DetailScraper(Scraper):
 
-    def __init__(self, day, season, driver, credentials, urls, classes, features,
-                 maps=None, transform=None):
-
-        super().__init__(driver, credentials, urls, classes, maps, transform)
-
-        self.day = day
-        self.season = season
+    def __init__(self, day, season, driver, credentials, table_name, urls, classes, features,
+                 mappings=None, transformations=None):
+        super().__init__(day, season, driver, table_name, credentials, urls, classes, mappings,
+                         transformations)
         self.features = features
 
     # GET LIST OF GAMES URL
