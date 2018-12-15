7f21e41aa5591f129f722158770e18501cc3ffe2
Raphaël Berly <raphaelberly@gmail.com>
2018-12-15 16:52:21 +0100

    Added resolve function to tools and removed transformation functions

--- lib/tools.py
+++ lib/tools.py
@@ -1,19 +1,24 @@
 import os
 from contextlib import contextmanager
 
-import numpy as np
-import pandas as pd
 
+def resolve(name):
+    """
+    Copied & pasted directly from the logging module code.
+    https://github.com/python/cpython/blob/master/Lib/logging/config.py
+    """
 
-def fraction_to_float(string):
-    # Handle NAs
-    if pd.isnull(string):
-        return np.nan
-    # Parse and check the input string
-    aux = string.split('/')
-    assert len(aux) in [1, 2], 'The provided string ("{}") is not a number or a fraction'.format(string)
-    # Output the computed ratio value as a string
-    return aux[0] if len(aux) == 1 else str(float(aux[0])/float(aux[1]))
+    name = name.split('.')
+    used = name.pop(0)
+    found = __import__(used)
+    for n in name:
+        used = used + '.' + n
+        try:
+            found = getattr(found, n)
+        except AttributeError:
+            __import__(used)
+            found = getattr(found, n)
+    return found
 
 
 @contextmanager
